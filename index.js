2938498597dbc0a8b21cad53956228990d052bdd
Muhammad <muhammadbutt9898@gmail.com>
2021-11-22 17:19:53 +0500

    chore: deploy config issue

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+const __dirname = path.resolve();
+
 import routes from "./routes/index.js";
 import { connectDB } from "./util/connection.js";
+
 const port = 5000;
 connectDB();
 const app = express();
@@ -12,6 +16,11 @@ app.use(
   })
 );
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
